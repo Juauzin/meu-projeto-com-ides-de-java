a9cdc4f31ef39ca355f656d9efa2756d1e875a98
Juauzin <rjoao3185@gmail.com>
2023-03-19 13:07:18 -0300

    First commit

---
+++ br/com/dio/PrimeiroJava.java
@@ -0,0 +1,23 @@
+package br.com.dio;
+
+import br.com.dio.model.Cat;
+
+public class PrimeiroJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	/*	int a=2;
+		int b=3;
+      System.out.println("Salve Mundo  ");
+	}*/
+Cat cat = new Cat();
+Livros Livros = new Livros();
+System.out.println(cat);
+System.out.println(Livros);
+}
+class Livros {
+	private String nome;
+	private String npage;
+	
+	
+}
